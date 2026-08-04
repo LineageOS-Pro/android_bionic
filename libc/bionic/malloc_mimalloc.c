86b3b98958a5da3371b3354468e84c99be4e02a1
xiaoka6666 <115566356+xiaoka6666@users.noreply.github.com>
2026-08-04 12:31:10 +0800

    libc: Adapt mimalloc wrapper to dev3's mi_theap_get_default
    mimalloc dev3 renamed mi_heap_get_default() to mi_theap_get_default();
    update the malloc_iterate best-effort visitor call accordingly.
    
    Change-Id: Id8dc1aed3f81d67550b02363e73f290f57482cdc

--- libc/bionic/malloc_mimalloc.c
+++ libc/bionic/malloc_mimalloc.c
@@ -139,7 +139,8 @@ int mi_malloc_iterate(uintptr_t base, size_t size,
     return -1;
   }
   struct MiIterateContext ctx = {base, size, callback, arg};
-  mi_heap_visit_blocks(mi_heap_get_default(), true /* visit_blocks */, MiBlockVisitor, &ctx);
+  // dev3 renamed mi_heap_get_default() to mi_theap_get_default().
+  mi_heap_visit_blocks(mi_theap_get_default(), true /* visit_blocks */, MiBlockVisitor, &ctx);
   return 0;
 }
 
